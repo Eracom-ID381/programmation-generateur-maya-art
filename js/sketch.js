a1ca5d4152a1777864ae60255c59ff89a612cd55
maya-art <60931149+maya-art@users.noreply.github.com>
2020-05-06 17:29:49 +0200

    Essai tête

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,45 @@
 // ----------------- GÉNÉRATEUR --------------------------- //
 
-// une unité = 1 seconde
-let timer = 1;
+
+
+
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
+    createCanvas(window.innerWidth, window.innerHeight);
+    strokeWeight(2);
+    rectMode(CENTER);
 }
 
 function draw() {
-  // Conditionellle nous permettant de savoir si la souris est appuyée 
-  // et si le timer est écoulé
-  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    generator(width / 2, height / 2, height-150);
-  }
+    background(255);
+
+    //face(width/2, height/2, 120, 30);
+    for (let x = 0; x < width, x = x + 100) {
+        face(x, height / 2, x / 100, 230)
+    }
 }
 
-// Fonction avec quatres paramètres
-function generator(x, y, size) {
-  // fill utilise le paramètre de notre fonction
-  for (let offset = size; offset > 0; offset -= 10) { 
-      fill(random(0,230));
-      rect(x, y, offset, offset);
-      console.log(offset);
-  }
+function face(x, y, bouche, couleur) {
+    fill(couleur);
+    ellipse(x, y, 200);
+    fill(255);
+    ellipse(x - 40, y - 40, 40, 40)
+    fill(0);
+    ellipse(x - 40, y - 40, 10, 10)
+    fill(255);
+    ellipse(x + 40, y - 40, 40, 40)
+    fill(0);
+    ellipse(x + 40, y - 40, 10, 10)
+    line(x - bouche, y + 50, x + bouche, y + 50);
+
 }
 
-function mousePressed() { 
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-  save();
+
+function mousePressed() {
+    // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
+    save();
 }
 
-function windowResized() { 
+function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
