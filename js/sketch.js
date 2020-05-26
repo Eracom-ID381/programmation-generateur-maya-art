58b94add5d03ac9beb2749c4bfc3ed81b4495f72
maya-art <60931149+maya-art@users.noreply.github.com>
2020-05-26 14:17:29 +0200

    Main

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,57 @@
 // ----------------- GÉNÉRATEUR --------------------------- //
-//une unité =1 seconde
+
+// une unité = 1 seconde
 let timer = 1;
+let bulle = new Bullle();
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    strokeWeight(2);
     rectMode(CENTER);
 }
 
 function draw() {
-
-
-    //face(width/2, height/2, 120, 30);
+    // Conditionellle nous permettant de savoir si la souris est appuyée
+    // et si le timer est écoulé
     if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-        background(255);
-        for (let x = 0; x < width; x = x + 100) {
-            face(x, height / 2, x / 100, 230)
-        }
+        // Appel de notre fonction generator
+        generator(width / 2, height / 2, height - 150);
     }
 }
+// Fonction avec quatres paramètres
+function generator(x, y, size) {
+    // fill utilise le paramètre de notre fonction
+    for (let offset = size; offset > 0; offset -= 60) {
+        fill(255, 300, 0);
+        ellipse(x, y, 244);
+        fill(255, 0, 0);
+        ellipse(x - 20, y - 80, 20, 20)
+        fill(0);
+        ellipse(x - 20, y - 80, 20, 20)
+        fill(255);
+        ellipse(x + 20, y - 60, 20, 20)
+        fill(0);
+        ellipse(x + 20, y - 60, 20, 20)
+        line(x - bouche, y + 10, x + bouche, y + 10);
+        console.log(offset);
+
+    }
+}
+
+
+//Nouvelle classe /Nouvelle Instance
+//class Bulle {
+//    constructor() {
+//        this.x = 10 / 2;
+//        this.y = height / 2;
+//    }
+//    bouger() {
+//        this.x = this.x + 10;
+//    }
+//}
+//}
+
+
+
 
 //EMOJIS
 
